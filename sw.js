1c39a92efe6abc29557930f5eb4e0b46233d39b5
박홍시 <107135571+P-hongsi@users.noreply.github.com>
2023-04-30 12:03:39 +0000

    1

---
+++ sw.js
@@ -0,0 +1,21 @@
+// Change this to your repository name
+var GHPATH = '/SuSi';
+
+// Choose a different app prefix name
+var APP_PREFIX = '수시';
+
+// The version of the cache. Every time you change any of the files
+// you need to change this version (version_01, version_02…). 
+// If you don't change the version, the service worker will give your
+// users the old files!
+var VERSION = 'V_0.1';
+
+// The files to make available for offline use. make sure to add 
+// others to this list
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/Bsu/Career_bsu.html`,
+  `${GHPATH}/Bsu/Common_bsu.html`,
+  `${GHPATH}/Bsu/Compare_bsu.html`
+]
