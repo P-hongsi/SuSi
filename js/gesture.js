23ee22b4e145a20ee360064956173ebfa696ca13
박홍시 <107135571+P-hongsi@users.noreply.github.com>
2023-06-03 14:26:17 +0000

    뒤로앞으로 제스처 추가

--- js/gesture.js
+++ js/gesture.js
@@ -1,55 +1,60 @@
 let startY1;
 let startY2;
-      
+let startX;
       document.addEventListener('touchstart', function(event) {
-        var touch = event.touches[0];
+        let touch = event.touches[0];
         if (event.touches.length === 3) {
-          var element2 = document.querySelector('#bsu');
+          const element2 = document.querySelector('#bsu');
           element2.click();
         }
         if (event.touches.length === 4) {
-          var element3 = document.querySelector('#bku');
+          const element3 = document.querySelector('#bku');
           element3.click();
         }
         if (event.touches.length === 5) {
-          var element4 = document.querySelector('.icon');
+          const element4 = document.querySelector('.icon');
           element4.click();
         }
         if (event.touches.length === 2) {
-          var touch1 = event.touches[0];
-          var touch2 = event.touches[1];
-          startY1 = Math.max(touch1.clientY, touch2.clientY); // 두 손가락 중 가장 아래에 있는 Y 좌표 저장
+          let touch1 = event.touches[0];
+          let touch2 = event.touches[1];
+          startY1 = Math.max(touch1.clientY, touch2.clientY);
+          const element = document.querySelector('.gesture');
+          element.click();
         }
-        startY2 = touch.clientY; // 터치 시작 Y 좌표 저장
+        startX = event.touches[0].clientX;
+        startY2 = touch.clientY;
       });
 
       document.addEventListener('touchmove', function(event) {
         if (event.touches.length === 2) {
-          var touch1 = event.touches[0];
-          var touch2 = event.touches[1];
+          let touch1 = event.touches[0];
+          let touch2 = event.touches[1];
 
-          var endY1 = Math.max(touch1.clientY, touch2.clientY); // 두 손가락 중 현재 가장 아래에 있는 Y 좌표
+          let endY1 = Math.max(touch1.clientY, touch2.clientY);
           if (endY1 > startY1) {
-            var element5 = document.querySelector('#close');
+            const element5 = document.querySelector('#close');
             element5.click();
           }
         }
       });
 
       document.addEventListener('touchend', function(event) {
-        var touch = event.changedTouches[0];
-        var endY2 = touch.clientY;
+        let touch = event.changedTouches[0];
+        let endY2 = touch.clientY;
         if (event.touches.length < 2) {
-          startY1 = null; // 터치가 끝나면 시작 Y 좌표 초기화
+          startY1 = null;
         }
         if (endY2 > startY2+160) {
-          var element1 = document.querySelector('.navbar__toogleBtn');
+          const element1 = document.querySelector('.navbar__toogleBtn');
           element1.click();
         }
+        let endX = event.changedTouches[0].clientX;
+        let deltaX = endX - startX;
+        console.log(deltaX);
+        if (deltaX > 150) {
+          window.history.back();
+        } else if (deltaX < -150) {
+          window.history.forward();
+        }
       });
-document.addEventListener('touchstart', function(event) {
-    if (event.touches.length === 2) {
-      const element = document.querySelector('.gesture');
-      element.click();
-    }
-});
